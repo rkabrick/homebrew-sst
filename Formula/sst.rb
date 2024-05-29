4b40339ba1fb4eba4c8769fcda2a61a93414065e
rkabrick <rkabrick@udel.edu>
2024-05-29 11:24:35 -0400

    Changing to 14.0.0

--- Formula/sst.rb
+++ Formula/sst.rb
@@ -1,12 +1,12 @@
 class Sst < Formula
   desc "Structural Simulation Toolkit (SST) Core and Elements"
   homepage "https://github.com/sstsimulator"
-  url "https://github.com/sstsimulator/sst-core/releases/download/v13.1.0_Final/sstcore-13.1.0.tar.gz"
-  version "13.1.0"
+  url "https://github.com/sstsimulator/sst-core/releases/download/v14.0.0_Final/sstcore-14.0.0.tar.gz"
+  version "14.0.0"
 
   resource "sst-elements" do
-    url "https://github.com/sstsimulator/sst-elements/releases/download/v13.1.0_Final/sstelements-13.1.0.tar.gz"
-    version "13.1.0"
+    url "https://github.com/sstsimulator/sst-elements/releases/download/v14.0.0_Final/sstelements-14.0.0.tar.gz"
+    version "14.0.0"
   end
 
   depends_on "autoconf" => :build
@@ -15,8 +15,7 @@ class Sst < Formula
   depends_on "python@3.10"
 
   def install
-    system "./configure", "PYTHON=#{Formula["python@3.10"].opt_bin}/python3.10",
-                          "--enable-debug",
+    system "./configure", "--enable-debug",
                           "--enable-profile",
                           "--disable-mpi",
                           "--disable-dependency-tracking",
@@ -27,8 +26,7 @@ class Sst < Formula
 
     # Now install sst-elements
     resource("sst-elements").stage do
-      system "./configure", "PYTHON=#{Formula["python@3.10"].opt_bin}/python3.10",
-                            "--enable-debug",
+      system "./configure", "--enable-debug",
                             "--enable-profile",
                             "--disable-mpi",
                             "--disable-dependency-tracking",
