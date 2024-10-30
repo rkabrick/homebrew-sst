a03f26e705eb1cc0ebd8fcb128af548932498dd6
rkabrick <rkabrick@udel.edu>
2024-10-30 12:41:11 -0400

    Adding default as 14.1.0

---
+++ Formula/sst.rb
@@ -0,0 +1,45 @@
+class SstAT1410 < Formula
+  desc "Structural Simulation Toolkit (SST) Core and Elements"
+  homepage "https://github.com/sstsimulator"
+  url "https://github.com/sstsimulator/sst-core/releases/download/v14.1.0_Final/sstcore-14.1.0.tar.gz"
+  version "14.1.0"
+
+  resource "sst-elements" do
+    url "https://github.com/sstsimulator/sst-elements/releases/download/v14.1.0_Final/sstelements-14.1.0.tar.gz"
+    version "14.1.0"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "ncurses" => :build
+  depends_on "python" => :build
+
+  def install
+    system "./configure", "--enable-debug",
+                          "--enable-profile",
+                          "--disable-mpi",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "-j#{ENV.make_jobs}"
+    system "make", "install"
+
+    resource("sst-elements").stage do
+      system "./configure", "--enable-debug",
+                            "--enable-profile",
+                            "--disable-mpi",
+                            "--disable-dependency-tracking",
+                            "--disable-silent-rules",
+                            "--prefix=#{prefix}",
+                            "--with-sst-core=#{prefix}"
+      system "make", "-j#{ENV.make_jobs}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/sst --version")
+    system "#{bin}/sst-info", "-q"
+  end
+end
