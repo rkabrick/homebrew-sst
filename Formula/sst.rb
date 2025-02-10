71b72f247839cc9bcc2bd2d68a27ac4917b2e2cd
rkabrick <rkabrick@udel.edu>
2025-02-10 15:52:39 -0500

    Adding head option to install latest source

--- Formula/sst.rb
+++ Formula/sst.rb
@@ -1,19 +1,29 @@
 class Sst < Formula
   desc "Structural Simulation Toolkit (SST) Core and Elements"
   homepage "https://github.com/sstsimulator"
+
+  # Stable release from a tarball
   url "https://github.com/sstsimulator/sst-core/releases/download/v14.1.0_Final/sstcore-14.1.0.tar.gz"
   version "14.1.0"
 
+  # HEAD build: clone the latest source from the main branch
+  head do
+    url "https://github.com/sstsimulator/sst-core.git", branch: "main"
+  end
+
   resource "sst-elements" do
+    # Stable release for sst-elements
     url "https://github.com/sstsimulator/sst-elements/releases/download/v14.1.0_Final/sstelements-14.1.0.tar.gz"
     version "14.1.0"
+    # HEAD build for sst-elements: clone from the main branch
+    head "https://github.com/sstsimulator/sst-elements.git", branch: "main"
   end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "libtool" => :build
-  depends_on "ncurses" => :build
-  depends_on "python" => :build
+  depends_on "libtool"  => :build
+  depends_on "ncurses"  => :build
+  depends_on "python"   => :build
 
   def install
     system "./configure", "--enable-debug",
