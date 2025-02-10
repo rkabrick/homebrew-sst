723cff2382b901e2e475af9274aae5cf4361547b
rkabrick <rkabrick@udel.edu>
2025-02-10 15:55:39 -0500

    Whoopsie

--- Formula/sst.rb
+++ Formula/sst.rb
@@ -2,30 +2,35 @@ class Sst < Formula
   desc "Structural Simulation Toolkit (SST) Core and Elements"
   homepage "https://github.com/sstsimulator"
 
-  # Stable release from a tarball
+  # Stable tarball for sst-core
   url "https://github.com/sstsimulator/sst-core/releases/download/v14.1.0_Final/sstcore-14.1.0.tar.gz"
   version "14.1.0"
 
-  # HEAD build: clone the latest source from the main branch
+  # HEAD build for sst-core: clone from the main branch
   head do
     url "https://github.com/sstsimulator/sst-core.git", branch: "main"
   end
 
   resource "sst-elements" do
-    # Stable release for sst-elements
-    url "https://github.com/sstsimulator/sst-elements/releases/download/v14.1.0_Final/sstelements-14.1.0.tar.gz"
-    version "14.1.0"
-    # HEAD build for sst-elements: clone from the main branch
-    head "https://github.com/sstsimulator/sst-elements.git", branch: "main"
+    if build.head?
+      # For HEAD builds, clone from the repository using Git
+      url "https://github.com/sstsimulator/sst-elements.git", using: :git, branch: "main"
+    else
+      # Stable tarball for sst-elements
+      url "https://github.com/sstsimulator/sst-elements/releases/download/v14.1.0_Final/sstelements-14.1.0.tar.gz"
+      version "14.1.0"
+    end
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool"  => :build
-  depends_on "ncurses"  => :build
-  depends_on "python"   => :build
+  depends_on "autoconf"  => :build
+  depends_on "automake"  => :build
+  depends_on "libtool"   => :build
+  depends_on "ncurses"   => :build
+  depends_on "python"    => :build
 
   def install
+    # Run autogen if building from HEAD (the stable tarball already includes the configure script)
+    system "./autogen.sh" if build.head?
     system "./configure", "--enable-debug",
                           "--enable-profile",
                           "--disable-mpi",
@@ -36,6 +41,7 @@ class Sst < Formula
     system "make", "install"
 
     resource("sst-elements").stage do
+      system "./autogen.sh" if build.head?
       system "./configure", "--enable-debug",
                             "--enable-profile",
                             "--disable-mpi",
